e2bc874f75ccb664e005fdd19d78598690d85ac2
Brandon Martinez <me@brandonmartinez.com>
2021-10-05 10:57:18 -0400

    Added portainer and pihole configurations

--- OS/SetupPiClusterOs-004-A.sh
+++ OS/SetupPiClusterOs-004-A.sh
@@ -8,5 +8,15 @@
 echo "Installing k3s"
 curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
+echo "Enabling Traefik Dashboard"
+echo "    dashboard:" | tee -a /var/lib/rancher/k3s/server/manifests/traefik.yaml > /dev/null
+echo "      enabled: true" | tee -a /var/lib/rancher/k3s/server/manifests/traefik.yaml > /dev/null
+
+echo "Installing Helm"
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+echo "Creating Kubernetes Support Directories"
+mkdir /var/lib/pihole
+
 echo "Finished Installing k3s - Use the Following Token to Add Nodes"
 cat /var/lib/rancher/k3s/server/token
---
+++ deploy-services.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+##################################################
+# Before running this script, copy the k3s.yaml  #
+# file from the remote cluster. You can do that  #
+# from the remote pi with this command:          #
+# cat /etc/rancher/k3s/k3s.yaml                  #
+# Also, copy the .env.sample to .env and update  #
+# the values any of your choosing                #
+##################################################
+
+set -e
+
+set -o allexport
+source .env
+set +o allexport
+
+export KUBECONFIG=$(pwd)/kubeconfig.yml
+
+function apply() {
+    echo "Replacing environment variables and applying $1 via kubectl"
+    envsubst < $1 | kubectl apply -f -
+}
+
+echo "Adding Rancher Local Path Provisioner"
+# From: https://github.com/rancher/local-path-provisioner/blob/master/README.md#usage
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+echo "Applying Portainer"
+kubectl apply -n portainer -f https://raw.githubusercontent.com/portainer/k8s/master/deploy/manifests/portainer/portainer-lb.yaml
+
+echo "Applying Namespaces"
+apply Services/01-namespaces.yml
+
+echo "Creating DNS Services"
+apply Services/02-dns.yml
+
+echo "Creating Pi-Hole Services"
+apply Services/03-pihole.yml
+
+echo "Configuring Ingress"
+apply Services/04-Ingress.yml
