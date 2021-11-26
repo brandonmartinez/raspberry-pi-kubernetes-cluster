ab9bc23cc0ee8f698822573b0007e37e5fc9f7a3
Brandon Martinez <me@brandonmartinez.com>
2021-11-26 01:19:37 -0500

    Updated pihole config to not cache in pihole. Removed local path provisioner - everything should be nfs

--- src/k8s/deploy.sh
+++ src/k8s/deploy.sh
@@ -18,13 +18,6 @@ function apply () {
 
 function deploy() {
     ##################################################
-    section "Adding Rancher Local Path Provisioner"
-    ##################################################
-    # From: https://github.com/rancher/local-path-provisioner/blob/master/README.md#usage
-    kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
-    kubectl patch storageclass "local-path" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-    
-    ##################################################
     section "Setting NFS as the Default Storage Class"
     ##################################################
     kubectl patch storageclass "nfs" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
