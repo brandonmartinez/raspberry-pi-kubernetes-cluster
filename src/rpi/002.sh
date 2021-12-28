9d79dcb268b5e982ec673b221c103682ae59374e
Brandon Martinez <me@brandonmartinez.com>
2021-12-28 00:06:59 -0500

    Added Static DNS to prevent DNS loop from pihole/unbound. Added checks to pihole

--- src/rpi/002.sh
+++ src/rpi/002.sh
@@ -12,6 +12,11 @@ source ../../.env
 source ../_shared/echo.sh
 set +o allexport
 
+section "Setting DNS to CloudFlare to Avoid Circular DNS"
+sed -i "s/#static domain_name_servers=192.168.1.1/static domain_name_servers=1.1.1.1 1.0.0.1/g" /etc/dhcpcd.conf
+systemctl daemon-reload
+service dhcpcd restart
+
 section "Updating Packages"
 apt-get update && apt-get -y upgrade
 
