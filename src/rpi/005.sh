72b1bf3ac7ce41d3d6d104cc890e23d3d3a9986f
Brandon Martinez <me@brandonmartinez.com>
2022-10-08 14:23:39 -0400

    Add taint to avoid master node for longhorn, setting up scaling for pihole

--- src/rpi/005.sh
+++ src/rpi/005.sh
@@ -8,9 +8,10 @@ source ../_shared/echo.sh
 source ../k8s/deploy.sh
 set +o allexport
 
-section "Adding Taint to Avoid Scheduling on Master Node"
-kubectl taint nodes $CLUSTER_HOSTNAME node-role.kubernetes.io/master:NoSchedule
+section "Addings Taint to Avoid Scheduling on Master Node"
 kubectl taint nodes $CLUSTER_HOSTNAME node-role.kubernetes.io/control-plane:NoSchedule
+kubectl taint nodes $CLUSTER_HOSTNAME node-role.kubernetes.io/master:NoSchedule
+kubectl taint nodes $CLUSTER_HOSTNAME cattle.io/os=linux:NoSchedule
 
 section "Adding ipv4Only Label to Cluster Master Node"
 kubectl label nodes $CLUSTER_HOSTNAME ipv4Only=true
