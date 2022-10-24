7504c2ad4a32d68fd467f987072cf10a40eb55a1
Brandon Martinez <me@brandonmartinez.com>
2022-10-24 16:58:23 -0400

    Adding conditional deployment

--- src/k8s/deploy.sh
+++ src/k8s/deploy.sh
@@ -46,36 +46,75 @@ function deploy_helm() {
 }
 
 function deploy() {
-    ##################################################
-    section "Installing Longhorn Storage Provider"
-    ##################################################
-    deploy_helm "longhorn" "https://charts.longhorn.io" \
-        "longhorn" "longhorn/longhorn" \
-        "bases/longhorn/helm-values.yml" \
-        "longhorn-system" \
-        120
+    if [ "$DEPLOY_LONGHORN" = true ] ; then
+        ##################################################
+        section "Installing Longhorn Storage Provider"
+        ##################################################
+        deploy_helm "longhorn" "https://charts.longhorn.io" \
+            "longhorn" "longhorn/longhorn" \
+            "bases/longhorn/helm-values.yml" \
+            "longhorn-system" \
+            120
 
-    ##################################################
-    section "Setting Longhorn as the Default Storage Class"
-    ##################################################
-    kubectl patch storageclass "local-path" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-    kubectl patch storageclass "nfs" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
-    kubectl patch storageclass "longhorn" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+        ##################################################
+        section "Setting Longhorn as the Default Storage Class"
+        ##################################################
+        kubectl patch storageclass "local-path" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+        kubectl patch storageclass "nfs" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+        kubectl patch storageclass "longhorn" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+    fi
 
-    ##################################################
-    section "Installing Prometheus Monitoring Stack"
-    ##################################################
+    if [ "$DEPLOY_PROMETHEUS" = true ] ; then
+        ##################################################
+        section "Installing Prometheus Monitoring Stack"
+        ##################################################
 
-    deploy_helm "prometheus-community" "https://prometheus-community.github.io/helm-charts" \
-        "monitoring" "prometheus-community/kube-prometheus-stack" \
-        "bases/prometheus/helm-values.yml" \
-        "monitoring"
+        deploy_helm "prometheus-community" "https://prometheus-community.github.io/helm-charts" \
+            "monitoring" "prometheus-community/kube-prometheus-stack" \
+            "bases/prometheus/helm-values.yml" \
+            "monitoring"
+    fi
 
     ##################################################
     section "Deploying Service Stacks"
     ##################################################
     log "Creating network services to be consumed by cluster and network-wide resources."
-    
+
+    log "Generating kustomize script"
+    echo -e "apiVersion: kustomize.config.k8s.io/v1beta1\nkind: Kustomization\n\nbases:" > kustomization.yml
+    echo "- bases/kube-system" >> kustomization.yml
+
+    if [ "$DEPLOY_LONGHORN" = true ] ; then
+        echo "- bases/longhorn" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_PIHOLE" = true ] ; then
+        echo "- bases/unbound" >> kustomization.yml
+        echo "- bases/pihole" >> kustomization.yml
+        echo "- bases/orbitalsync" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_HOMEBRIDGE" = true ] ; then
+        echo "- bases/homebridge" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_DEEPSTACK" = true ] ; then
+        echo "- bases/deepstack" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_PORTAINER" = true ] ; then
+        echo "- bases/portainer" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_CHRONY" = true ] ; then
+        echo "- bases/chrony" >> kustomization.yml
+    fi
+
+    if [ "$DEPLOY_MINECRAFT" = true ] ; then
+        echo "- bases/minecraft" >> kustomization.yml
+    fi
+
+    log "Deploying kustomize script via kubectl"
     # This is hack for the prometheus stack, as "$" is used in rules and dashboard definitions
     # When adding new templates, be sure to replace "$" with "${DOLLAR}" to avoid invalid YAML
     export DOLLAR='$'
