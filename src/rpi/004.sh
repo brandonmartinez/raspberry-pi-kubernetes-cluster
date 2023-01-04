0d957ab50e80478265f6fc7237fabf79c2a3249a
Brandon Martinez <me@brandonmartinez.com>
2023-01-04 13:53:15 -0500

    fixing some issues with prometheus reporting

--- src/rpi/004.sh
+++ src/rpi/004.sh
@@ -30,5 +30,5 @@ then
   cat /var/lib/rancher/k3s/server/token
 else
   section "Installing k3s Node"
-  curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.24 K3S_URL=https://$PRIMARY_IP:6443 K3S_TOKEN=$TOKEN sh -
+  curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.24 K3S_URL=https://$PRIMARY_IP:6443 K3S_TOKEN=$TOKEN sh -s - --kube-proxy-arg 'metrics-bind-address=0.0.0.0'
 fi
