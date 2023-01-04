ab43109c5f5f804a3e0bf91d038fd2de3c78967e
Brandon Martinez <me@brandonmartinez.com>
2023-01-04 00:32:58 -0500

    Added optional mounting of USB drive

--- src/rpi/001.sh
+++ src/rpi/001.sh
@@ -21,5 +21,23 @@ section "Setting Pi Password"
 section "Expanding File System"
 raspi-config --expand-rootfs
 
+if [ "$MOUNT_USB" = true ] ; then
+    if [ "$MOUNT_USB_DRIVE_FORMAT" = true ] ; then
+        section "Formatting attached USB Drive $MOUNT_USB_DRIVE_PATH with GPT partition table and ext4 volume"
+        wipefs -af "$MOUNT_USB_DRIVE_PATH"
+        sgdisk -n 0:0:0 -t 0:8300 -c 0:data "$MOUNT_USB_DRIVE_PATH"
+
+        mkfs.ext4 -F "${MOUNT_USB_DRIVE_PATH}1"
+    fi
+
+    section "Mounting attached USB Drive ${MOUNT_USB_DRIVE_PATH}1 to $MOUNT_USB_MOUNT_PATH"
+    mkdir -p "$MOUNT_USB_MOUNT_PATH"
+    mount "${MOUNT_USB_DRIVE_PATH}1" "$MOUNT_USB_MOUNT_PATH"
+    
+    section "Setting up FSTAB entry for USB Drive"
+    MOUNT_USB_UUID=$(lsblk -no UUID "${MOUNT_USB_DRIVE_PATH}1")
+    echo "UUID=$MOUNT_USB_UUID $MOUNT_USB_MOUNT_PATH ext4 defaults,auto,users,rw,nofail 0 0" | tee -a /etc/fstab
+fi
+
 section "Rebooting"
 reboot
