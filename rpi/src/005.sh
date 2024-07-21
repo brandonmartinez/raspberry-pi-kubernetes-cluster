2fce99f3ce46e2bd05e50cb4053fcd576699b3b1
Brandon Martinez <me@brandonmartinez.com>
2024-07-21 00:01:00 -0400

    Switched to docker compose for scrypted

--- rpi/src/005.sh
+++ rpi/src/005.sh
@@ -13,7 +13,7 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 section "Addings Taint to Avoid Scheduling on Master Node"
 kubectl taint nodes $CLUSTER_HOSTNAME node-role.kubernetes.io/control-plane:NoSchedule --overwrite
 kubectl taint nodes $CLUSTER_HOSTNAME node-role.kubernetes.io/master:NoSchedule --overwrite
-if [ "$MOUNT_USB" = false ] ; then
+if [ "$MOUNT_USB" = false ]; then
     kubectl taint nodes $CLUSTER_HOSTNAME cattle.io/os=linux:NoSchedule --overwrite
 fi
 
@@ -30,17 +30,17 @@ deploy
 section "Starting Watchtower to Keep Docker Images Updated"
 
 docker run -d \
-  --name watchtower \
-  --restart unless-stopped \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower:latest \
-  --interval 3600 --cleanup
+    --name watchtower \
+    --restart unless-stopped \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    containrrr/watchtower:latest \
+    --interval 3600 --cleanup
 
 section "Starting HomeBridge in Docker"
 
 HOMEBRIDGE_DATA_DIRECTORY="/home/pi/homebridge"
 
-if [ "$MOUNT_USB" = true ] ; then
+if [ "$MOUNT_USB" = true ]; then
     HOMEBRIDGE_DATA_DIRECTORY="$MOUNT_USB_MOUNT_PATH/homebridge"
 fi
 
@@ -51,20 +51,3 @@ docker run -d \
     --net=host --name=homebridge \
     -e PGID=1000 -e PUID=1000 -e HOMEBRIDGE_CONFIG_UI=1 -e HOMEBRIDGE_CONFIG_UI_PORT=8581 -e TZ=America/Detroit \
     -v "$HOMEBRIDGE_DATA_DIRECTORY:/homebridge" oznu/homebridge:latest
-
-section "Starting Scrypted in Docker"
-
-SCRYPTED_DATA_DIRECTORY="/home/pi/scrypted"
-
-if [ "$MOUNT_USB" = true ] ; then
-    SCRYPTED_DATA_DIRECTORY="$MOUNT_USB_MOUNT_PATH/scrypted"
-fi
-
-mkdir -p "$SCRYPTED_DATA_DIRECTORY"
-
-docker run -d \
-    --restart unless-stopped \
-    --net=host --name=scrypted \
-    -e PGID=1000 -e PUID=1000 -e TZ=America/Detroit \
-    -v "$SCRYPTED_DATA_DIRECTORY:/server/volume" koush/scrypted:latest
-
